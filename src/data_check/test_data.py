9f8c0b8e33999ac33ca2020f5f740e2fe04ff980
e.ghodhbani <e.ghodhbani@instadeep.com>
2022-03-27 22:32:36 +0100

    added data_ckeck pipeline

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -61,5 +61,15 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 
 
 ########################################################
-# Implement here test_row_count and test_price_range   #
+def test_row_count(data):
+    """
+    Check whether the dataset consists of the raisonnable number of rows
+    """
+    assert 15000 < data.shape[0] < 1000000
+    
+def test_price_range(data, min_price, max_price):
+    """
+    Check whether the prices are in the range of min and max prices
+    """
+    assert data['price'].between(min_price, max_price).all()
 ########################################################
